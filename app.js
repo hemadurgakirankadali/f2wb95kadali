b11a50e71c92d125cc4d2f56bc8a25ee45ef3cfa
hemadurgakirankadali <112112171+hemadurgakirankadali@users.noreply.github.com>
2022-11-02 22:40:45 -0500

    bonus

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var earbudsRouter = require('./routes/earbuds');
 var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/earbuds', earbudsRouter);
 app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
